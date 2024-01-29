5c73597434c1b3d676a13e230428d63a6d56058e
hamzahcodes <shaikhhamzah93@gmail.com>
2024-01-29 19:36:58 +0530

    as said by git

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   daisyui: {
-    themes: ["emerald","pastel","winter","acid","fantasy","garden","luxury","retro","halloween","wireframe","synthwave"],
+    // themes: ["emerald","pastel","winter","acid","fantasy","garden","luxury","retro","halloween","wireframe","synthwave"],
+    // themes: false,
+    themes: [
+      {
+        mytheme: {
+          "secondary": "#6366f1",
+          "primary": "rgba(243,244,246,0.75)",
+          "accent": "#6366f1",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
   },
   theme: {
     extend: {
