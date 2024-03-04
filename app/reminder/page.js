c5ea50338be33a55f3dfaf0d8e600243bee95b3a
hamzahcodes <shaikhhamzah93@gmail.com>
2024-03-04 23:11:20 +0530

    reminder message page

--- app/reminder/page.js
+++ app/reminder/page.js
@@ -6,25 +6,27 @@ import { useQuery } from "@tanstack/react-query";
 import { getReminderAmount } from "@helper/http";
 
 const page = () => {
-
-    const [ amount, setAmount ] = useState("");
     
     const searchParams = useSearchParams()
     const custID = searchParams.get('custID')
     const userID = searchParams.get('userID')
-
+    console.log(custID, " ", userID);
+    console.log("inside reminder page");
     const { data, error, isPending, isError } = useQuery({
         queryKey: ["reminder"],
         queryFn: () => getReminderAmount({custID: custID, userID: userID }),
     });
 
+    console.log(data);
 
   return (
-    <>
-    {data && 
-        <div className='text-white'>Your Payment amount is left for : {data[0]?.reminderAmount
-        }</div>}
-    </>
+    <div className='text-center'>
+      <div className='text-gray-900 text-3xl font-medium'>Payment Reminder for</div>
+      {data && 
+        <div className='text-red-600 text-3xl my-4 font-medium'>{"₹ " + data[0]?.reminderAmount}</div> 
+      }<div className='text-gray-900 text-2xl font-normal'>Sent by Al-Hadi Goat Farm</div>
+        <div className='text-gray-900 text-xl my-2 font-light	'>Sajid Mulla | 8850284856</div>
+    </div>
   )
 }
 
